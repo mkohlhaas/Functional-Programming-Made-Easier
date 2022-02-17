46a41f565bdc1d2c2072afac4141a643f8ccad4e
Hans-J. Schmid <knock@myopendoor.de>
2022-02-17 09:04:27 +0100

    ex-ch-07-b

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,39 @@
 module Main where
 
-import Prelude
-
+import Prelude (Unit, discard)
+import Data.Maybe (Maybe)
 import Effect (Effect)
 import Effect.Console (log)
 
+-----------------
+-- Data Types --
+-----------------
+
+newtype CSV = CSV String
+newtype FullName = FullName String
+newtype Age = Age Int
+data Occupation = Doctor | Dentist | Lawyer | Unemployed
+data Person = Person { name :: FullName, age :: Age, occupation :: Occupation }
+
+------------------
+-- Type Classes --
+------------------
+
+class ToCSV a where
+  toCSV :: a -> CSV
+
+class FromCSV a where
+  fromCSV :: CSV -> Maybe a
+
+----------
+-- Main --
+----------
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log "Chapter 7."
+  log "Convert Person to CSV and back again."
+  -- log $ show $ toCSV (Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }) == CSV "Sue Smith,23,Doctor"
+  -- log $ show $ toCSV (Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }) -- "Sue Smith,23,Doctor"
+  -- let person = Person { name: FullName "Sue Smith" , age: Age 23 , occupation: Doctor }
+  -- log $ show $ (toCSV person # fromCSV) == Just person
