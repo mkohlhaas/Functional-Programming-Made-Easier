f639ca70c592a9d80a9bf5040ff09e6f928acdd2
Hans-J. Schmid <knock@myopendoor.de>
2022-02-17 08:13:08 +0100

    ex-ch-07-a1

---
+++ src/Main.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var unsafeCompareImpl = function (lt) {
+  return function (eq) {
+    return function (gt) {
+      return function (x) {
+        return function (y) {
+          return x < y ? lt : x === y ? eq : gt;
+        };
+      };
+    };
+  };
+};
+
+var refEq = function (r1) {
+  return function (r2) {
+    return r1 === r2;
+  };
+};
+
+exports.showIntImpl = function (n) {
+  return n.toString();
+};
+
+exports.showStringImpl = function (s) {
+  var l = s.length;
+  return "\"" + s.replace(
+    /[\0-\x1F\x7F"\\]/g, // eslint-disable-line no-control-regex
+    function (c, i) {
+      switch (c) {
+        case "\"":
+        case "\\":
+          return "\\" + c;
+        case "\x07": return "\\a";
+        case "\b": return "\\b";
+        case "\f": return "\\f";
+        case "\n": return "\\n";
+        case "\r": return "\\r";
+        case "\t": return "\\t";
+        case "\v": return "\\v";
+      }
+      var k = i + 1;
+      var empty = k < l && s[k] >= "0" && s[k] <= "9" ? "\\&" : "";
+      return "\\" + c.charCodeAt(0).toString(10) + empty;
+    }
+  ) + "\"";
+};
+
+exports.eqIntImpl = refEq;
+exports.ordIntImpl = unsafeCompareImpl;
