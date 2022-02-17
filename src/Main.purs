8097ff65251d1bad7a7a2b53f00be8aac2695749
Hans-J. Schmid <knock@myopendoor.de>
2022-02-17 08:40:26 +0100

    ex-ch-07-a2

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,37 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, discard, show, (==), (<=), (<), (>), (>=), ($))
 
+import Data.Eq (class Eq)
+import Data.Ord (class Ord)
+import Data.Show (class Show)
 import Effect (Effect)
 import Effect.Console (log)
 
+------------------------------------------------------------------
+-- 1. Define data types Maybe and Either. (Do not import them!) --
+------------------------------------------------------------------
+
+----------
+-- Main --
+----------
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log "Chapter 7."
+  log "Derive missing instances!"
+  -- log $ show $ Just 5 == Just 5          -- true
+  -- log $ show $ Just 5 == Just 2          -- false
+  -- log $ show $ Just 5 == Nothing         -- false
+  -- log $ show $ Nothing == Just 5         -- false
+  -- log $ show $ Nothing == Nothing        -- true
+  -- log $ show $ Left "left"               -- (Left "left")
+  -- log $ show $ Right (Just 42)           -- (Right (Just 42))
+  -- log $ show $ Just 1 < Just 5           -- true
+  -- log $ show $ Just 5 <= Just 5          -- true
+  -- log $ show $ Just 5 > Just 10          -- false
+  -- log $ show $ Just 10 >= Just 10        -- true
+  -- log $ show $ Just 99 > Nothing         -- true
+  -- log $ show $ Just 99 < Nothing         -- false
+  -- log $ show $ Just "abc"                -- (Just "abc")
+  -- log $ show $ (Nothing :: Maybe Unit)   -- Nothing
