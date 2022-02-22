a0991833e46d720b3578647214507155dc4c3a04
Hans-J. Schmid <knock@myopendoor.de>
2022-02-22 07:54:42 +0100

    removes some superfluous do\'s.

--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,14 @@ module Main where
 import Prelude
 import Effect (Effect)
 
-import Ch22a as Ch22a
--- import Ch22b as Ch22b
--- import Ch22c as Ch22c
--- import Ch22d as Ch22d
--- import Ch22e as Ch22e
--- import Ch22f as Ch22f
--- import Ch22g as Ch22g
--- import Ch22h as Ch22h
+import Ch22a as Ch22
+-- import Ch22b as Ch22
+-- import Ch22c as Ch22
+-- import Ch22d as Ch22
+-- import Ch22e as Ch22
+-- import Ch22f as Ch22
+-- import Ch22g as Ch22
+-- import Ch22h as Ch22
 
 main :: Effect Unit
-main = do
-  Ch22a.test
+main = Ch22.test
