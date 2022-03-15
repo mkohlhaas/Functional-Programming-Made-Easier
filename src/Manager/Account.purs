70df45b487ca39a16ac7a2e68c9a0b9a585dceed
Hans-J. Schmid <knock@myopendoor.de>
2022-03-15 23:47:36 +0100

    finishes 28.7.2.

--- src/Manager/Account.purs
+++ src/Manager/Account.purs
@@ -1,3 +1,19 @@
 module Manager.Account where
 
 import Prelude
+
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Tuple (Tuple(..))
+import Effect.Aff (Aff)
+import Effect.Aff.AVar (AVar)
+import Effect.Aff.AVar as AVar
+import Entity.Account (Account(..))
+
+type Accounts = Map String Account
+
+startup :: Array Account -> Aff (AVar Accounts)
+startup accounts = (accounts <#> \account@(Account { userName }) -> Tuple userName account) # Map.fromFoldable # AVar.new
+
+shutdown :: AVar Accounts -> Aff Unit
+shutdown = void <<< AVar.take
