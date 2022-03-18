b1109985d6ab7c5d228a9e746ab7a6ddbfc88009
Hans-J. Schmid <knock@myopendoor.de>
2022-03-18 17:06:10 +0100

    finishes chapter 28.9.

--- src/Main.purs
+++ src/Main.purs
@@ -4,14 +4,19 @@ import Prelude
 
 import Control.Monad.Reader (runReaderT)
 import Data.Array (head)
+import Data.DateTime (diff)
 import Data.Either (Either(..), hush)
+import Data.JSDate (fromDateTime, toUTCString)
 import Data.Maybe (Maybe(..))
 import Data.NonEmpty (oneOf, (:|))
 import Data.Posix.Signal (Signal(..))
+import Data.Time.Duration (Milliseconds)
+import Data.UUID (genUUID)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
+import Effect.Now (nowDateTime)
 import HTTPure (toString)
 import HTTPure as HTTPure
 import HTTPure.Request (Request)
@@ -21,7 +26,21 @@ import Handler.Api.Logon (Logon)
 import Handler.Class.ApiHandler (HandlerEnv, handle)
 import Manager.Account as AccountManager
 import Node.Process (onSignal)
+import Record (delete)
 import Type.Proxy (Proxy(..))
+
+loggingRouter :: HandlerEnv -> Request -> ResponseM
+loggingRouter env req = do
+  id <- liftEffect genUUID
+  let idStr = " SessionID: " <> show id <> " "
+      ts date = toUTCString $ fromDateTime date
+  startDate <- liftEffect nowDateTime
+  log $ "REQUEST: " <> ts startDate <> idStr <> (show $ delete (Proxy :: _ "body") req)
+  res <- router env req
+  endDate <- liftEffect nowDateTime
+  let duration = " [Duration: " <> show (diff endDate startDate :: Milliseconds) <> " ms]"
+  log $ "RESPONSE: " <> ts endDate <> idStr <> (show $ delete (Proxy :: _ "writeBody") res) <> duration
+  pure res
 
 router :: HandlerEnv -> Request -> ResponseM
 router env { body, method }
@@ -43,7 +62,7 @@ main = launchAff_ do
     Right accounts -> do
       accountsAVar <- AccountManager.startup accounts
       liftEffect $ do
-        shutdown <- HTTPure.serve port (router { accountsAVar }) $ log $ "Server up running on port: " <> show port
+        shutdown <- HTTPure.serve port (loggingRouter { accountsAVar }) $ log $ "Server up running on port: " <> show port
         let
           shutdownServer = do
             log "Shutting down server..."
