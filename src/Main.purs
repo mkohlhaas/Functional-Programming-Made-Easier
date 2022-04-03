61e9bddaa0db3bc65bed17e4e52af61b179a1a7d
Hans-J. Schmid <knock@myopendoor.de>
2022-04-03 20:36:37 +0200

    adds solution to 2nd exercise of chapter 7.

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,39 @@
 module Main where
 
-import Prelude (Unit, discard, show, (==), (<=), (<), (>), (>=), ($))
-
 import Data.Eq (class Eq)
+import Data.Generic.Rep (class Generic)
 import Data.Ord (class Ord)
 import Data.Show (class Show)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
+import Prelude (Unit, discard, show, (==), (<=), (<), (>), (>=), ($))
 
 -------------------------------------------------------------
 -- Define data types Maybe and Either. Do not import them! --
 -------------------------------------------------------------
+
+----------------
+-- Data Types --
+----------------
+
+data Maybe a = Nothing | Just a
+data Either a b = Left a | Right b
+
+---------------------
+-- Class Instances --
+---------------------
+
+derive instance Eq a => Eq (Maybe a)
+derive instance (Eq a, Eq b) => Eq (Either a b)
+derive instance Ord a => Ord (Maybe a)
+derive instance (Ord a, Ord b) => Ord (Either a b)
+derive instance Generic (Maybe a) _
+derive instance Generic (Either a b) _
+instance Show a => Show (Maybe a) where
+  show = genericShow
+instance (Show a, Show b) => Show (Either a b) where
+  show = genericShow
 
 ----------
 -- Main --
@@ -20,18 +43,20 @@ main :: Effect Unit
 main = do
   log "Exercise Chapter 7."
   log "Derive missing instances! Only necessary imports are allowed."
-  log $ show $ Just 5 == Just 5          -- true
-  log $ show $ Just 5 == Just 2          -- false
-  log $ show $ Just 5 == Nothing         -- false
-  log $ show $ Nothing == Just 5         -- false
-  log $ show $ Nothing == Nothing        -- true
-  log $ show $ Left "left"               -- (Left "left")
-  log $ show $ Right (Just 42)           -- (Right (Just 42))
-  log $ show $ Just 1 < Just 5           -- true
-  log $ show $ Just 5 <= Just 5          -- true
-  log $ show $ Just 5 > Just 10          -- false
-  log $ show $ Just 10 >= Just 10        -- true
-  log $ show $ Just 99 > Nothing         -- true
-  log $ show $ Just 99 < Nothing         -- false
-  log $ show $ Just "abc"                -- (Just "abc")
-  log $ show $ (Nothing :: Maybe Unit)   -- Nothing
+  log $ show $ Just 5 == Just 5                     -- true
+  log $ show $ Just 5 == Just 2                     -- false
+  log $ show $ Just 5 == Nothing                    -- false
+  log $ show $ Nothing == Just 5                    -- false
+  log $ show $ Nothing == (Nothing :: Maybe Unit)   -- true
+  log $ show $ (Left "left" :: Either _ Unit)       -- (Left "left")
+  log $ show $ (Right (Just 42) :: Either Unit _)   -- (Right (Just 42))
+  log $ show $ Left 1 < (Left 2 :: Either _ Unit)   -- true
+  log $ show $ Left 2 < (Left 1 :: Either _ Unit)   -- false
+  log $ show $ Just 1 < Just 5                      -- true
+  log $ show $ Just 5 <= Just 5                     -- true
+  log $ show $ Just 5 > Just 10                     -- false
+  log $ show $ Just 10 >= Just 10                   -- true
+  log $ show $ Just 99 > Nothing                    -- true
+  log $ show $ Just 99 < Nothing                    -- false
+  log $ show $ Just "abc"                           -- (Just "abc")
+  log $ show $ (Nothing :: Maybe Unit)              -- Nothing
