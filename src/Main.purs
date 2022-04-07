221c862ba72750995831d090d442249d09175f58
Hans-J. Schmid <knock@myopendoor.de>
2022-04-07 03:58:35 +0200

    makes findMax more generic and adds test cases.

--- src/Main.purs
+++ src/Main.purs
@@ -27,7 +27,7 @@ max :: ∀ a. Ord a => a -> a -> a
 max x y | x > y = x
         | otherwise = y
 
-findMax :: ∀ a. Ord a => a -> List a -> a
+findMax :: ∀ f a. Foldable f => Ord a => a -> f a -> a
 findMax = foldl max
 
 findMaxNE :: ∀ a. Ord a => NonEmptyList a -> a
@@ -79,10 +79,12 @@ main = do
   log $ show $ max "aa" "z"                                                                    -- "z"
   log $ show $ findMax 0  (37 : 311 : -1 : 2 : 84 : Nil)                                       -- 311 (0  is default value)
   log $ show $ findMax "" ("a" : "bbb" : "c" : Nil)                                            -- "c" ("" is default value)
-  log $ show $ findMaxNE (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil))                      -- 311
-  log $ show $ findMaxNE (NonEmptyList $ "a" :| ("bbb" : "c" : Nil))                           -- "c"
+  log $ show $ findMaxNE (NonEmptyList $ 37 :| 311 : -1 : 2 : 84 : Nil)                        -- 311
+  log $ show $ findMaxNE (NonEmptyList $ "a" :| "bbb" : "c" : Nil)                             -- "c"
   log $ show $ sum (1   : 2   : 3   : Nil)                                                     -- 6
   log $ show $ sum (1.0 : 2.0 : 3.0 : Nil)                                                     -- 6.0
+  log $ show $ sum [1   , 2   , 3 ]                                                            -- 6
+  log $ show $ sum [1.0 , 2.0 , 3.0 ]                                                          -- 6.0
   log $ show $ toList          (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- (5 : -1 : 14 : 99 : Nil)
   log $ show $ sum             (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- 117
   log $ show $ toList $ LFTree (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- (5 : -1 : 14 : 99 : Nil)
