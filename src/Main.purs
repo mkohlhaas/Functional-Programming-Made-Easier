894c45200ac743f6aca257dc46b6b3572c702436
Hans-J. Schmid <knock@myopendoor.de>
2022-04-08 20:09:09 +0200

    makes an isomorphism.

--- src/Main.purs
+++ src/Main.purs
@@ -8,38 +8,38 @@ import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 
--- data Iso a b = Iso (a -> b) (b -> a)
+positive :: Maybe Int -> Maybe Int
+positive x = if x >= Just 0 then x else Nothing
 
--- iso :: ∀ a b f. Invariant f => Iso a b -> f a -> f b
--- iso (Iso to from) = imap to from
+positiveEither' :: Either Unit Int -> Either Unit Int
+positiveEither' (Right x) = positive (Just x) # note unit
+positiveEither' (Left _) = positive Nothing # note unit
 
--- isoMaybeEither :: String -> Iso (Maybe Int) (Either String Int)
--- isoMaybeEither err = Iso (note err) hush
+data Iso a b = Iso (a -> b) (b -> a)
+
+isoMaybeEither :: Iso (Maybe Int) (Either Unit Int)
+isoMaybeEither = Iso (note unit) hush
 
 newtype Endo a = Endo (a -> a)
 
 instance Invariant Endo where
   imap ab ba (Endo aa) = Endo (ab <<< aa <<< ba)
 
-positive :: Maybe Int -> Maybe Int
-positive x = if x >= Just 0 then x else Nothing
+iso :: ∀ a b f. Invariant f => Iso a b -> f a -> f b
+iso (Iso to from) = imap to from
 
-positiveEither :: Either String Int -> Either String Int
+positiveEither :: Either Unit Int -> Either Unit Int
 positiveEither = f
   where
-  (Endo f) = imap (note "Not a positive integer") hush (Endo positive)
-
-positiveEither' :: Either String Int -> Either String Int
-positiveEither' (Right x) = positive (Just x) # note "Not a positive integer"
-positiveEither' (Left _) = positive Nothing # note "Not a positive integer"
+  (Endo f) = iso isoMaybeEither (Endo positive)
 
 main :: Effect Unit
 main = do
-  log $ show $ positive (Just 5)                                      -- (Just 5)
-  log $ show $ positive (Just (-1))                                   -- Nothing
-  log $ show $ positiveEither $ Right 5                               -- (Right 5)
-  log $ show $ positiveEither $ Left "Error!"                         -- (Left "Not a positive integer")
-  log $ show $ positive (Just 5) # note "Not a positive integer"      -- (Right 5)
-  log $ show $ positive (Just (-1)) # note "Not a positive integer"   -- (Left "Not a positive integer")
-  log $ show $ positiveEither' $ Right 5                              -- (Right 5)
-  log $ show $ positiveEither' $ Left "Error!"                        -- (Left "Not a positive integer")
+  log $ show $ positive (Just 5)                                                    -- (Just 5)
+  log $ show $ positive (Just (-1))                                                 -- Nothing
+  log $ show $ positiveEither $ Right 5                                             -- (Right 5)
+  log $ show $ positiveEither $ Left unit                                           -- (Left unit)
+  log $ show $ positive (Just 5) # note unit                                        -- (Right 5)
+  log $ show $ positive (Just (-1)) # note unit                                     -- (Left unit)
+  log $ show $ positiveEither' $ Right 5                                            -- (Right 5)
+  log $ show $ positiveEither' $ Left unit                                          -- (Left unit)
