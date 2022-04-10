4c8d64279fde6d83dc85a39fc5e50aea7806b0d3
Hans-J. Schmid <knock@myopendoor.de>
2022-04-10 03:29:44 +0200

    adds error message example.

--- spago.dhall
+++ spago.dhall
@@ -1,5 +1,5 @@
 { name = "my-project"
-, dependencies = [ "console", "effect", "prelude" ]
+, dependencies = [ "console", "effect", "either", "maybe", "prelude" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,28 @@ module Main where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
 
+fullName :: String -> String -> String -> String
+fullName first middle last = first <> " " <> middle <> " " <> last
+
+errIfMissing :: Maybe String -> String -> Either String String
+errIfMissing Nothing err = Left err
+errIfMissing (Just s) _ = Right s
+
+fullNameEither :: Maybe String -> Maybe String -> Maybe String -> Either String String
+fullNameEither first middle last =
+  fullName
+    <$> (first `errIfMissing` "First name missing.")
+    <*> (middle `errIfMissing` "Middle name missing.")
+    <*> (last `errIfMissing` "Last name missing.")
+
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ show $ fullNameEither (Just "Hans") (Just "Fritz") (Just "Schmid")
+  log $ show $ fullNameEither Nothing (Just "Fritz") (Just "Schmid")
+  log $ show $ fullNameEither (Just "Hans") Nothing (Just "Schmid")
+  log $ show $ fullNameEither (Just "Hans") (Just "Fritz") Nothing
