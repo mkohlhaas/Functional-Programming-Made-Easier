22abfa0a3f78f7fb5c22e58cb5cd53438f83c461
Hans-J. Schmid <knock@myopendoor.de>
2022-04-13 18:35:08 +0200

    uncomments tests and reformats source code.

--- src/Main.purs
+++ src/Main.purs
@@ -34,30 +34,34 @@ import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "Ch. 19."
-  -- log "-- Maybe Monad --"
-  -- log $ show $ Just (_ * 10) <*> Just 20
-  -- log $ show $ Just (_ * 10) <*> pure 20
-  -- log $ show $ Just 20 >>= pure <<< (_ * 10)
-  -- log $ show do
-  --    x <- Just 20
-  --    let y = x * 10
-  --    pure y
-  -- log $ show $ Just 20 >>= const Nothing >>= \y -> Just $ y + 42
-  -- log $ show do
-  --    _ <- Just 20
-  --    y <- Nothing
-  --    pure $ y + 42
-  -- log "-- Either Monad --"
--- log $ show $ Right (_ * 10) <*> (Right 20 :: Either Unit _)
--- log $ show $ Right (_ * 10) <*> (pure 20 :: Either Unit _)
--- log $ show $ (Right 20 :: Either Unit _) >>= pure <<< (_ * 10)
--- log $ show do
---   x <- Right 20 :: Either Unit _
---   let y = x * 10
---   pure y
--- log $ show $ Right 20 >>= const (Left "error") >>= \y -> Right $ y + 42
--- log $ show do
---   _ <- Right 20
---   y <- Left "error"
---   pure $ y + 42
+  log "Exercise Chapter 19."
+  log "-----------------"
+  log "-- Maybe Monad --"
+  log "-----------------"
+  log $ show $ Just (_ * 10) <*> Just 20
+  log $ show $ Just (_ * 10) <*> pure 20
+  log $ show $ Just 20 >>= pure <<< (_ * 10)
+  log $ show do
+    x <- Just 20
+    let y = x * 10
+    pure y
+  log $ show $ Just 20 >>= const Nothing >>= \y -> Just $ y + 42
+  log $ show do
+    _ <- Just 20
+    y <- Nothing
+    pure $ y + 42
+  log "------------------"
+  log "-- Either Monad --"
+  log "------------------"
+  log $ show $ Right (_ * 10) <*> (Right 20 :: Either Unit _)
+  log $ show $ Right (_ * 10) <*> (pure 20 :: Either Unit _)
+  log $ show $ (Right 20 :: Either Unit _) >>= pure <<< (_ * 10)
+  log $ show do
+    x <- Right 20 :: Either Unit _
+    let y = x * 10
+    pure y
+  log $ show $ Right 20 >>= const (Left "error") >>= \y -> Right $ y + 42
+  log $ show do
+    _ <- Right 20
+    y <- Left "error"
+    pure $ y + 42
