a9ab541943d52d7a50ace7b6aab362a98bedd3fd
Hans-J. Schmid <knock@myopendoor.de>
2022-07-13 00:03:02 +0200

    aligns comments.

--- src/Main.purs
+++ src/Main.purs
@@ -40,9 +40,9 @@ main = do
   log "------------------------------------"
   log "-- Applicative Instance for Maybe --"
   log "------------------------------------"
-  log $ show $ (+) <$> Just 21 <*> Just 21             -- (Just 42)
-  log $ show $ (*) <$> pure 2 <*> pure 21              -- (Just 42)
-  log $ show $ pure (+) <*> Just 17 <*> Just 25        -- (Just 42)
+  log $ show $ (+) <$> Just 21 <*> Just 21 -------------- (Just 42)
+  log $ show $ (*) <$> pure 2 <*> pure 21 --------------- (Just 42)
+  log $ show $ pure (+) <*> Just 17 <*> Just 25 --------- (Just 42)
   log "-------------------------------------"
   log "-- Applicative Instance for Either --"
   log "-------------------------------------"
@@ -57,8 +57,8 @@ main = do
   log "----------------"
   log "-- Validation --"
   log "----------------"
-  log $ show $ createFamilyAges { fatherAge: Age 40, motherAge: Age 30, childAge: Age 10 }  -- (Validation (Right (FamilyAges { childAge: 10, fatherAge: 40, motherAge: 30 })))
+  log $ show $ createFamilyAges { fatherAge: Age 40, motherAge: Age 30, childAge: Age 10 } --- (Validation (Right (FamilyAges { childAge: 10, fatherAge: 40, motherAge: 30 })))
   log $ show $ createFamilyAges { fatherAge: Age 400, motherAge: Age 300, childAge: Age 0 } -- (Validation (Left ["Father is too old", "Mother is too old", "Child is too young"]))
-  log $ show $ createFamilyAges { fatherAge: Age 4, motherAge: Age 3, childAge: Age 10 }    -- (Validation (Left ["Father is too young", "Mother is too young"]))
+  log $ show $ createFamilyAges { fatherAge: Age 4, motherAge: Age 3, childAge: Age 10 } ----- (Validation (Left ["Father is too young", "Mother is too young"]))
   log $ show $ createFamilyAges { fatherAge: Age 40, motherAge: Age 30, childAge: Age 100 } -- (Validation (Left ["Child is too old"]))
-  log $ show $ createFamilyAges { fatherAge: Age 40, motherAge: Age 3, childAge: Age 0 }    -- (Validation (Left ["Mother is too young", "Child is too young"]))
+  log $ show $ createFamilyAges { fatherAge: Age 40, motherAge: Age 3, childAge: Age 0 } ----- (Validation (Left ["Mother is too young", "Child is too young"]))
