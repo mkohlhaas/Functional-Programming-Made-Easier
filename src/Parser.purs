589d0a79aa77e1e1275200837278815e94254ba8
Hans-J. Schmid <knock@myopendoor.de>
2022-07-12 03:04:30 +0200

    changes Alt instance.

--- src/Parser.purs
+++ src/Parser.purs
@@ -57,10 +57,7 @@ instance Bind (Parser e) where
 instance Monad (Parser e)
 
 instance Alt (Parser e) where
-  alt p1 p2 =
-    Parser \s -> case parse p1 s of
-      Left _ -> parse p2 s
-      Right x -> Right x
+  alt p1 p2 = Parser \s -> parse p1 s <|> parse p2 s
 
 instance Lazy (Parser e a) where
   defer f = Parser \str -> parse (f unit) str
