5f76e67a05bb9d1a2fad7ab15c3855c051190cd7
Hans-J. Schmid <knock@myopendoor.de>
2022-07-12 23:49:42 +0200

    aligns comments.

--- src/Main.purs
+++ src/Main.purs
@@ -24,20 +24,20 @@ main :: Effect Unit
 main = do
   log "Exercise Chapter 7."
   log "Derive missing instances! Only necessary imports are allowed."
-  log $ show $ Just 5 == Just 5     -- true
-  log $ show $ Just 5 == Just 2     -- false
-  log $ show $ Just 5 == Nothing    -- false
-  log $ show $ Nothing == Just 5    -- false
-  log $ show $ Nothing == Nothing   -- true
-  log $ show $ Left "left"          -- (Left "left")
-  log $ show $ Right (Just 42)      -- (Right (Just 42))
-  log $ show $ Left 1 < Left 2      -- true
-  log $ show $ Left 2 < Left 1      -- false
-  log $ show $ Just 1 < Just 5      -- true
-  log $ show $ Just 5 <= Just 5     -- true
-  log $ show $ Just 5 > Just 10     -- false
-  log $ show $ Just 10 >= Just 10   -- true
-  log $ show $ Just 99 > Nothing    -- true
-  log $ show $ Just 99 < Nothing    -- false
-  log $ show $ Just "abc"           -- (Just "abc")
-  log $ show $ Nothing              -- Nothing
+  log $ show $ Just 5 == Just 5 ------ true
+  log $ show $ Just 5 == Just 2 ------ false
+  log $ show $ Just 5 == Nothing ----- false
+  log $ show $ Nothing == Just 5 ----- false
+  log $ show $ Nothing == Nothing ---- true
+  log $ show $ Left "left" ----------- (Left "left")
+  log $ show $ Right (Just 42) ------- (Right (Just 42))
+  log $ show $ Left 1 < Left 2 ------- true
+  log $ show $ Left 2 < Left 1 ------- false
+  log $ show $ Just 1 < Just 5 ------- true
+  log $ show $ Just 5 <= Just 5 ------ true
+  log $ show $ Just 5 > Just 10 ------ false
+  log $ show $ Just 10 >= Just 10 ---- true
+  log $ show $ Just 99 > Nothing ----- true
+  log $ show $ Just 99 < Nothing ----- false
+  log $ show $ Just "abc" ------------ (Just "abc")
+  log $ show $ Nothing --------------- Nothing
