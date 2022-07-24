59950b0761945f75e878a831abcda00252fe2b05
Hans-J. Schmid <knock@myopendoor.de>
2022-07-24 02:26:15 +0200

    uses Unicode.

--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,12 @@ module Main where
 
 import Data.Eq (class Eq)
 import Data.Foldable (intercalate)
-import Data.Generic.Rep (class Generic)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.Show (class Show)
-import Data.Show.Generic (genericShow)
-import Data.String (Pattern(..), split)
+import Data.String.Common (split)
+import Data.String.Pattern (Pattern(..))
+import Data.String.Read (class Read, read)
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (Unit, bind, discard, pure, show, (#), ($), (<$>), (==))
@@ -20,81 +20,87 @@ newtype CSV = CSV String
 newtype FullName = FullName String
 newtype Age = Age Int
 data Occupation = Doctor | Dentist | Lawyer | Unemployed
-data Person = Person { name :: FullName, age :: Age, occupation :: Occupation }
+data Person = Person { name ∷ FullName, age ∷ Age, occupation ∷ Occupation }
 
 ------------------
 -- Type Classes --
 ------------------
 
 class ToCSV a where
-  toCSV :: a -> CSV
+  toCSV ∷ a → CSV
 
 class FromCSV a where
-  fromCSV :: CSV -> Maybe a
+  fromCSV ∷ CSV → Maybe a
 
 --------------------------
--- Type Class Instances --
+-- Type Class instances --
 --------------------------
 
+--------
+-- Eq --
+--------
 derive instance Eq CSV
-derive instance Eq FullName
 derive instance Eq Age
+derive instance Eq FullName
 derive instance Eq Occupation
 derive instance Eq Person
-derive instance Generic Occupation _
 
+----------
+-- Read --
+----------
+instance Read Age where
+  read str = Age <$> fromString str
+
+derive newtype instance Read FullName
+
+instance Read Occupation where
+  read "Doctor" = Just Doctor
+  read "Dentist" = Just Dentist
+  read "Lawyer" = Just Lawyer
+  read "Unemployed" = Just Unemployed
+  read _ = Nothing
+
+instance Read Person where
+  read p = case split (Pattern ",") p of
+    [ name', age', occupation' ] → do
+      name ← read name'
+      age ← read age'
+      occupation ← read occupation'
+      pure $ Person { name, age, occupation }
+    _ → Nothing
+
+----------
+-- Show --
+----------
 derive newtype instance Show CSV
+derive newtype instance Show Age
 
 instance Show FullName where
   show (FullName name) = name
 
-derive newtype instance Show Age
-
 instance Show Occupation where
-  show = genericShow
+  show Doctor = "Doctor"
+  show Dentist = "Dentist"
+  show Lawyer = "Lawyer"
+  show Unemployed = "Unemployed"
 
-instance Show Person where
-  show (Person { name, age, occupation }) = intercalate "," [ show name, show age, show occupation ]
-
+----------------
+-- To/FromCSV --
+----------------
 instance ToCSV Person where
-  toCSV person = CSV $ show person
+  toCSV (Person { name, age, occupation }) = CSV $ intercalate "," [ show name, show age, show occupation ]
 
 instance FromCSV Person where
-  fromCSV (CSV str) = readPerson str
-
----------------
--- Functions --
----------------
-
-readPerson :: String -> Maybe Person
-readPerson str = case split (Pattern ",") str of
-  [ name', age', occupation' ] -> do
-    let name = FullName name'
-    age <- readAge age'
-    occupation <- readOccupation occupation'
-    pure $ Person { name, age, occupation }
-  _ -> Nothing
-
-readAge :: String -> Maybe Age
-readAge str = Age <$> fromString str
-
-readOccupation :: String -> Maybe Occupation
-readOccupation = case _ of
-  "Doctor" -> Just Doctor
-  "Dentist" -> Just Dentist
-  "Lawyer" -> Just Lawyer
-  "Unemployed" -> Just Unemployed
-  _ -> Nothing
+  fromCSV (CSV p) = read p
 
 ----------
 -- Main --
 ----------
-
-main :: Effect Unit
+main ∷ Effect Unit
 main = do
   log "Chapter 7."
   log "Convert Person to CSV and back again. Imports are allowed and necessary."
-  log $ show $ toCSV (Person { name: FullName "Sue Smith", age: Age 23, occupation: Doctor }) == CSV "Sue Smith,23,Doctor"  -- true
-  log $ show $ toCSV (Person { name: FullName "Sue Smith", age: Age 23, occupation: Doctor })                               -- "Sue Smith,23,Doctor"
+  log $ show $ toCSV (Person { name: FullName "Sue Smith", age: Age 23, occupation: Doctor }) -------------------------------- "Sue Smith,23,Doctor"
+  log $ show $ toCSV (Person { name: FullName "Sue Smith", age: Age 23, occupation: Doctor }) == CSV "Sue Smith,23,Doctor" --- true
   let person = Person { name: FullName "Sue Smith", age: Age 23, occupation: Doctor }
-  log $ show $ (toCSV person # fromCSV) == Just person                                                                      -- true
+  log $ show $ (toCSV person # fromCSV) == Just person ----------------------------------------------------------------------- true
