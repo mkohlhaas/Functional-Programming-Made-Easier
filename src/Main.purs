b6b0a1f4a577080257aea5d2c82b8c9ccb018e97
Hans-J. Schmid <knock@myopendoor.de>
2022-07-25 01:02:54 +0200

    uses Unicode.

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,5 @@
 module Main where
 
-import Data.Boolean (otherwise)
 import Data.Foldable (class Foldable, foldlDefault, foldrDefault, foldl, foldMap)
 import Data.List (List(..), (:), singleton)
 import Data.List.NonEmpty (NonEmptyList(..))
@@ -14,26 +13,26 @@ newtype RFTree a = RFTree (Tree a) -- RightFirstTree; breadth-first search
 newtype LFTree a = LFTree (Tree a) -- LeftFirstTree; depth-first search
 
 class ToList f where
-  toList :: ∀ a. f a -> List a
+  toList ∷ ∀ a. f a → List a
 
 ---------------
 -- Functions --
 ---------------
 
-reverse :: List ~> List
+reverse ∷ List ~> List
 reverse = foldl (flip (:)) Nil
 
-max :: ∀ a. Ord a => a -> a -> a
-max x y | x > y = x
-        | otherwise = y
+max ∷ ∀ a. Ord a ⇒ a → a → a
+max x y = if x > y then x else y
 
-findMax :: ∀ f a. Foldable f => Ord a => a -> f a -> a
+findMax ∷ ∀ f a. Foldable f ⇒ Ord a ⇒ a → f a → a
 findMax = foldl max
 
-findMaxNE :: ∀ a. Ord a => NonEmptyList a -> a
-findMaxNE (NonEmptyList (a :| as)) = findMax a as
+findMaxNE ∷ ∀ a. Ord a ⇒ NonEmptyList a → a
+findMaxNE (NonEmptyList (x :| xs)) = findMax x xs
 
-sum :: ∀ a f. Foldable f => Semiring a => f a -> a
+-- Note: `Semiring` is a bit overkill as it also implies multiplication.
+sum ∷ ∀ a f. Foldable f ⇒ Semiring a ⇒ f a → a
 sum = foldl (+) zero
 
 -------------------------
@@ -71,23 +70,24 @@ instance ToList Tree where
 -- Main --
 ----------
 
-main :: Effect Unit
+main ∷ Effect Unit
 main = do
   log "Exercise Chapter 11."
-  log $ show $ reverse (10 : 20 : 30 : Nil)                                                    -- (30 : 20 : 10 : Nil)
-  log $ show $ max (-1) 99                                                                     -- 99
-  log $ show $ max "aa" "z"                                                                    -- "z"
-  log $ show $ findMax 0  (37 : 311 : -1 : 2 : 84 : Nil)                                       -- 311 (0  is default value)
-  log $ show $ findMax "" ("a" : "bbb" : "c" : Nil)                                            -- "c" ("" is default value)
-  log $ show $ findMaxNE (NonEmptyList $ 37 :| 311 : -1 : 2 : 84 : Nil)                        -- 311
-  log $ show $ findMaxNE (NonEmptyList $ "a" :| "bbb" : "c" : Nil)                             -- "c"
-  log $ show $ sum (1   : 2   : 3   : Nil)                                                     -- 6
-  log $ show $ sum (1.0 : 2.0 : 3.0 : Nil)                                                     -- 6.0
-  log $ show $ sum [1   , 2   , 3 ]                                                            -- 6
-  log $ show $ sum [1.0 , 2.0 , 3.0 ]                                                          -- 6.0
-  log $ show $ toList          (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- (5 : -1 : 14 : 99 : Nil)
-  log $ show $ sum             (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- 117
-  log $ show $ toList $ LFTree (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- (5 : -1 : 14 : 99 : Nil)
-  log $ show $ sum    $ LFTree (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- 117
-  log $ show $ toList $ RFTree (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- (99 : 14 : -1 : 5 : Nil)
-  log $ show $ sum    $ RFTree (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99))   -- 117
+  log "Using folds!!!"
+  log $ show $ reverse (10 : 20 : 30 : Nil) ------------------------------------------------------------ (30 : 20 : 10 : Nil)
+  log $ show $ max (-1) 99 ----------------------------------------------------------------------------- 99
+  log $ show $ max "aa" "z" ---------------------------------------------------------------------------- "z"
+  log $ show $ findMax 0 (37 : 311 : -1 : 2 : 84 : Nil) ------------------------------------------------ 311 (0  is default value)
+  log $ show $ findMax "" ("a" : "bbb" : "c" : Nil) ---------------------------------------------------- "c" ("" is default value)
+  log $ show $ findMaxNE (NonEmptyList $ 37 :| (311 : -1 : 2 : 84 : Nil)) ------------------------------ 311
+  log $ show $ findMaxNE (NonEmptyList $ "a" :| ("bbb" : "c" : Nil)) ----------------------------------- "c"
+  log $ show $ sum (1 : 2 : 3 : Nil) ------------------------------------------------------------------- 6
+  log $ show $ sum (1.0 : 2.0 : 3.0 : Nil) ------------------------------------------------------------- 6.0
+  log $ show $ sum [ 1, 2, 3 ] ------------------------------------------------------------------------- 6
+  log $ show $ sum [ 1.0, 2.0, 3.0 ] ------------------------------------------------------------------- 6.0
+  log $ show $ toList {-          -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- (5 : -1 : 14 : 99 : Nil)
+  log $ show $ sum {-             -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- 117
+  log $ show $ toList $ LFTree {- -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- (5 : -1 : 14 : 99 : Nil)
+  log $ show $ sum $ LFTree {-    -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- 117
+  log $ show $ toList $ RFTree {- -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- (99 : 14 : -1 : 5 : Nil)
+  log $ show $ sum $ RFTree {-    -}  (Node (Node (Leaf 5) (Node (Leaf (-1)) (Leaf 14))) (Leaf 99)) ---- 117
