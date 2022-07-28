8a5194ddd63699b1c27526d33c12993075aa7a51
Hans-J. Schmid <knock@myopendoor.de>
2022-07-28 23:01:57 +0200

    cleanses code.

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,12 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Except.Trans (ExceptT, runExceptT)
-import Control.Monad.Writer.Trans (WriterT, runWriterT)
-import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Control.Monad.Except.Trans (class MonadError, class MonadThrow, ExceptT, catchError, runExceptT, throwError)
+import Control.Monad.Reader.Trans (class MonadAsk, ask)
+import Control.Monad.State.Trans (class MonadState, get, put)
+import Control.Monad.Trans.Class (class MonadTrans, lift)
+import Control.Monad.Writer.Trans (class MonadTell, WriterT, runWriterT, tell)
+import Data.Either (Either)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
@@ -15,7 +17,7 @@ import Effect.Console (log)
 -- 2. Write runStateT.
 
 -- 3. Write Functor instance.
--- Note: m should be a Functor not a Monad to minimize constraints!
+-- Note: Only Functor constraint is allowed!
 
 -- 4. Write Apply instance.
 
@@ -39,37 +41,32 @@ import Effect.Console (log)
 
 -- 14. Write MonadError instance.
 
+-------------
+-- Testing --
+-------------
+
 -- This is our Monad stack:
 type AppStack e w s a = ExceptT e (WriterT w (StateT s Effect)) a
-type AppM = AppStack String String Int Unit
 
--- 15. Write runApp to run AppM.
+-- 15. Write runApp to run AppStack.
 
 -- 16. Factor out the return type, call it StackResult and update runApp.
--- type StackResult = ...
 
--- AppEffects stores all side-effects of the monad stack.
-type AppEffects = { log ∷ String, state ∷ Int, result ∷ Maybe Unit }
+-- Given helper function.
+logM ∷ ∀ m. MonadTell (Array String) m ⇒ String → m Unit
+logM s = tell [ s <> "\n" ]
 
--- AppResult contains our side-effect values from running our Monad Stack AppEffects and, optionally, the error if one occurred.
-type AppResult = Tuple (Maybe String) AppEffects
+-- This is our specific monad stack:
+type AppM = AppStack String (Array String) Int Unit
 
--- 17. Write a mapping function "results" that turns StackResult into an AppResult. Change runApp using "results".
--- results ∷ StackResult → AppResult
-
--- 18. Write helper function "logM" that appends a newline after every string in the writer.
-
--- 19. Write the application monad.
--- For logging use logM.
--- app ∷ AppM
-
--- The body of "app" contains the following actions in the specified order:
--- a) Write to the log "Starting App...".
--- b) Get the State.
--- c) Check the State to make sure it is non-zero, otherwise error with "WE CANNOT HAVE 0 STATE!".
--- d) Add 1 to the State.
--- e) Write to the log "Incremented State".
--- f) Return the Pure Computational Value.
+app ∷ AppM
+app = do
+  logM "Starting App..."
+  n ← get
+  when (n == 0) $ void $ throwError "WE CANNOT HAVE A ZERO STATE!"
+  put $ n + 1
+  logM "Incremented State"
+  pure unit
 
 ----------
 -- Main --
@@ -79,6 +76,7 @@ main ∷ Effect Unit
 main = do
   log "Exercise Chapter 21."
   result1 ← runApp 0 app
-  log $ show result1 ------------- (Tuple (Just "WE CANNOT HAVE A ZERO STATE!") { log: "Starting App...\n", result: Nothing, state: 0 })
+  -- We could transform the output to something nicer, but ... we call it a day!
+  log $ show result1 ------------- (Tuple (Tuple (Left "WE CANNOT HAVE A ZERO STATE!") ["Starting App...\n"]) 0)
   result2 ← runApp 99 app
-  log $ show result2 ------------- (Tuple Nothing { log: "Starting App...\nIncremented State\n", result: (Just unit), state: 100 })
+  log $ show result2 ------------- (Tuple (Tuple (Right unit) ["Starting App...\n","Incremented State\n"]) 100)
