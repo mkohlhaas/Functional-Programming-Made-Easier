59be06de6173bd06faf9b06973986655ad87562e
Hans-J. Schmid <knock@myopendoor.de>
2022-11-29 13:57:10 +0100

    cleans up the whole mess.
    Using named patterns and update syntax for records.

--- src/Main.purs
+++ src/Main.purs
@@ -6,58 +6,72 @@ import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
 
-type RWSResult r w s = { r ∷ r, w ∷ w, s ∷ s } ------------------------ contains all needed information
+type RWSResult r w s = { r ∷ r, w ∷ w, s ∷ s } ------------------------ contains a Reader, a Writer and a State
 data RWS r w s a = RWS (RWSResult r w s → Tuple a (RWSResult r w s)) -- works similar to the State monad
 
--- 1. Write Reader, Writer and State monads for your own reference.
+-- 1. Write Reader, Writer and State monads for your own reference. (They won't be used, though.)
+
 newtype Reader r a = Reader (r → a)
 newtype Writer w a = Writer (Tuple a w)
 newtype State s a = State (s → Tuple a s)
 
 -- 2. Write a runRWS function similar to runstate.
+
 runRWS ∷ ∀ r w s a. RWS r w s a → RWSResult r w s → Tuple a (RWSResult r w s)
 runRWS (RWS f) = f
 
--- 3. Write a functor instance for RWS.
+-- Note: Instances could be written in terms of runRWS but I find it more complicated.
+
+-- 3. Write a Functor instance for RWS.
+
 instance Functor (RWS r w s) where
-  map f x = RWS \rws → runRWS x rws # \(Tuple x' rws') → Tuple (f x') rws'
+  map f (RWS g) = RWS $ \rwsRes → g rwsRes # \(Tuple a rwsRes') → Tuple (f a) rwsRes'
 
--- 4. Write an apply instance for RWS. (Take shortcut possibly after you'll have written `bind` instance.)
-instance Monoid w ⇒ Apply (RWS r w s) where
-  apply = ap
+-- 4. Write an Apply instance for RWS.
 
--- 5. Write an applicative instance for RWS.
-instance Monoid w ⇒ Applicative (RWS r w s) where
-  pure x = RWS \{ r, s } → Tuple x { r, w: mempty, s }
+instance Apply (RWS r w s) where
+  apply (RWS f) (RWS g) = RWS $ \rwsRes → f rwsRes # \(Tuple f' rwsRes') → g rwsRes' # \(Tuple a rwsRes'') → Tuple (f' a) rwsRes''
 
--- 6. Write a bind instance for RWS.
-instance Monoid w ⇒ Bind (RWS r w s) where
-  bind x f = RWS \rws → runRWS x rws #
-    \(Tuple x' rws'@{ w: w' }) → runRWS (f x') rws' #
-      \(Tuple x'' rws''@{ w: w'' }) → Tuple x'' rws'' { w = w' <> w'' }
+-- instance Apply (RWS r w s) where
+--   apply = ap
 
--- 7. Write a monad instance for RWS.
-instance Monoid w ⇒ Monad (RWS r w s)
+-- 5. Write an Applicative instance for RWS.
+
+instance Applicative (RWS r w s) where
+  pure a = RWS $ \rwsRes → Tuple a rwsRes
+
+-- 6. Write a Bind instance for RWS.
+
+instance Bind (RWS r w s) where
+  bind (RWS f) g = RWS $ \rwsRes → f rwsRes # (\(Tuple a rwsRes') → g a # (\(RWS h) → h rwsRes'))
+
+-- 7. Write a Monad instance for RWS.
+
+instance Monad (RWS r w s)
 
 -------------------
 -- RWS Monad API --
 -------------------
 
 -- 8. Write a tell function.
+
 tell ∷ ∀ r w s. Semigroup w ⇒ w → RWS r w s Unit
-tell w = RWS \{ r, s } → Tuple unit { r, w, s }
+tell w = RWS $ \rwsRes@{ w: w' } → Tuple unit rwsRes { w = w' <> w }
 
 -- 9. Write an ask function.
-ask ∷ ∀ r w s. Monoid w ⇒ RWS r w s r
-ask = RWS \{ r, s } → Tuple r { r, w: mempty, s }
+
+ask ∷ ∀ r w s. RWS r w s r
+ask = RWS $ \rwsRes@{ r } → Tuple r rwsRes
 
 -- 10. Write a get function.
-get ∷ ∀ r w s. Monoid w ⇒ RWS r w s s
-get = RWS \{ r, s } → Tuple s { r, w: mempty, s }
+
+get ∷ ∀ r w s. RWS r w s s
+get = RWS $ \rwsRes@{ s } → Tuple s rwsRes
 
 -- 11. Write a put function.
-put ∷ ∀ r w s. s → Monoid w ⇒ RWS r w s Unit
-put s = RWS \{ r } → Tuple unit { r, w: mempty, s }
+
+put ∷ ∀ r w s. s → RWS r w s Unit
+put s = RWS $ \rwsRes → Tuple unit rwsRes { s = s }
 
 ---------------------------------
 -- Data Structures for Testing --
